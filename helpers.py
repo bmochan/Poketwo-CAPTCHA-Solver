c2031af473b6bc2f985148692a7b8f3c64ebf9b4
bmochan <108970739+bmochan@users.noreply.github.com>
2022-11-07 12:11:21 +0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -13,11 +13,12 @@ import requests
 from pydub import AudioSegment
 import speech_recognition as sr
 
+
 def SolveCaptcha(url: str) -> bool:
     # Defining chrome options
     chrome_options = Options()
-    chrome_options.add_argument("--disable-extensions")
-
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    
     # Initialising chrome
     chrome = uc.Chrome(browser_executable_path="C:\Program Files\Google\Chrome\Application\chrome.exe",
                        use_subprocess=True, chrome_options=chrome_options)
@@ -33,13 +34,13 @@ def SolveCaptcha(url: str) -> bool:
         audio_selector = '#recaptcha-audio-button'
         download_selector = '#rc-audio > div.rc-audiochallenge-tdownload > a'
 
-        # Wait for CAPTCHA to be visible
+        # Wait for CAPTCHA iframe to be visible
         WebDriverWait(chrome, 20).until(EC.frame_to_be_available_and_switch_to_it(
             (By.CSS_SELECTOR, "iframe[title='reCAPTCHA']")))
         captcha = WebDriverWait(chrome, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, captcha_selector)))
 
-        time.sleep(3)
+        time.sleep(2)
 
         # Click the captcha
         captcha.click()
@@ -52,7 +53,7 @@ def SolveCaptcha(url: str) -> bool:
         audio_btn = WebDriverWait(chrome, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, audio_selector)))
 
-        time.sleep(3)
+        time.sleep(2)
 
         # Click the audio button
         audio_btn.click()
@@ -65,52 +66,68 @@ def SolveCaptcha(url: str) -> bool:
         WebDriverWait(chrome, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, download_selector)))
 
+        time.sleep(2)
         # Get audio download link
         download_link = chrome.find_element(
             By.CSS_SELECTOR, download_selector).get_attribute("href")
 
-    except TimeoutException:
+        # Save the audio to disk as an MP3
+        r = requests.get(download_link)
+        with open('sound.mp3', 'wb') as f:
+            f.write(r.content)
+
+        # Convert audio to WAV and save to disk
+        # ffmpeg must be a PATH variable for this operation
+        sound = AudioSegment.from_mp3("sound.mp3")
+        sound.export("sound.wav", format="wav")
+
+        sample_audio = sr.AudioFile('sound.wav')
+
+        # Comprehend the audio
+        recognizer = sr.Recognizer()
+        audio = None
+
+        with sample_audio as source:
+            audio = recognizer.record(source)
+
+        text = recognizer.recognize_google(audio)
+
+        # Input the text from speech
+        input_box = chrome.find_element(By.ID, 'audio-response')
+
+        time.sleep(2)
+
+        input_box.send_keys(text.lower())
+        input_box.send_keys(Keys.ENTER)
+
+    except Exception:
+        chrome.close()
         return False
-
-    # Save the audio to disk as an MP3
-    r = requests.get(download_link)
-    with open('sound.mp3', 'wb') as f:
-        f.write(r.content)
-
-    # Convert audio to WAV and save to disk
-    # ffmpeg must be a PATH variable for this operation
-    sound = AudioSegment.from_mp3("sound.mp3")
-    sound.export("sound.wav", format="wav")
-
-    sample_audio = sr.AudioFile('sound.wav')
-
-    # Comprehend the audio
-    recognizer = sr.Recognizer()
-    audio = None
-
-    with sample_audio as source:
-        audio = recognizer.record(source)
-
-    text_keys = recognizer.recognize_google(audio)
-
-    # Input the text from speech
-    input_box = chrome.find_element(By.ID, 'audio-response')
-
-    input_box.send_keys(text_keys.lower())
-    input_box.send_keys(Keys.ENTER)
 
     # Get fail text; successful operation otherwise
     try:
-        #Wait for fail text
-        w = WebDriverWait(chrome, 10).until(
+        # Wait for fail text
+        WebDriverWait(chrome, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, '#rc-audio > div.rc-audiochallenge-error-message')))
 
         fail_text = chrome.find_element(
             By.CSS_SELECTOR, '#rc-audio > div.rc-audiochallenge-error-message').text
+
         if len(fail_text) > 0:
+            chrome.close()
             return False
 
     except TimeoutException:
+        # CAPTCHA was solved
+        chrome.switch_to.default_content()
+
+        verify_btn = chrome.find_element(By.TAG_NAME, "button")
+
+        time.sleep(2)        
+
+        # Finally, click the verify button
+        verify_btn.click()
+
         return True
 
 
